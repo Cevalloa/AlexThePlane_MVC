24ea10e2196095b15a8aa3d00ac38752be9d77b5
Alex Cevallos <Alex.Cevallos@alaskaair.com>
2017-04-20 12:15:26 -0700

    Added first flight path from USA to Australia on map

--- AlexThePlane/Scripts/MapFeatures.js
+++ AlexThePlane/Scripts/MapFeatures.js
@@ -234,7 +234,16 @@
                 strokeWeight: 2
             });
 
+            var uluru = { lat: -25.363, lng: 131.044 };
+
+            var marker = new google.maps.Marker({
+                position: uluru,
+                map: map
+            });
+
             flightPath.setMap(map);
+
+             
     }
 
 
